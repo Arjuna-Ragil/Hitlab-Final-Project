ef0755ae8b212dd39bb963031713ef05409d8d32
Arjuna-Ragil <arjunaragilputera@gmail.com>
2024-11-09 17:11:37 +0700

    added js and a few css

---
+++ landing page/landingPage.js
@@ -0,0 +1,8 @@
+//Tombol Login//
+function signUp() {
+  location.href = "/Hitlab-Final-Project/sign up page/signUpPage.html";
+}
+
+function signIn() {
+  location.href = "/Hitlab-Final-Project/sign in page/signInPage.html";
+}
