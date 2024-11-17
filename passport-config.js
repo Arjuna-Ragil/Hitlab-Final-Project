e0d814913f6b0d7d3dfe6b7795db04d39c0b32ba
Arjuna-Ragil <arjunaragilputera@gmail.com>
2024-11-17 20:09:38 +0700

    backend part 2 again part 2

---
+++ passport-config.js
@@ -0,0 +1,33 @@
+const { authenticate } = require('passport')
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateAccount = async (email, password, done) => {
+    const account = getUserByEmail(email)
+    if (account == null) {
+      return done(null, false, {message: 'No Account is connected to this email'})
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, account)
+      }
+      else {
+        return done(null, false, {message: 'password incorrect'})
+      }
+    }
+    catch (e) {
+      return done(e)
+    }
+  }
+
+  const localStrategy = require('passport-local')
+  .Strategy
+  passport.use(new localStrategy({ usernameField: 'email'}, authenticateAccount))
+  passport.serializeUser((user, done) => done(null, account.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
+}
+
+module.exports = initialize
